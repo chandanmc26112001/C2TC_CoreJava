630b1050440e86bcec2aabc6f2220d82f239211c
ADMIN <ADMIN@DESKTOP-PP33HSR>
2023-02-07 12:43:10 +0530

    This is first commit

---
+++ Loops/src/com/cg/loops/Loops.java
@@ -0,0 +1,15 @@
+package com.cg.loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		int x=10;
+		int i=1;
+		while(i<=10) {
+			System.out.println(x+"*"+i+"="+x*i);
+			i=i+1;
+		}
+
+	}
+
+}
